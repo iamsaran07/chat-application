2de4cda4ab99017619549f5b2952f0614781468c
Saran <166163944+iamsaran07@users.noreply.github.com>
2024-04-06 01:40:52 +0530

    Update mongo.js

--- config/mongo.js
+++ config/mongo.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const connect = mongoose.connect(process.env.MONGO_URL);
+// const connect = mongoose.connect(process.env.MONGO_URL);
 
-connect.then(()=>{
-    console.log("CONNECTED TO DATABASE...");
-})
-.catch((err)=>{
-    console.log("FAILED TO CONNECT DATABASE...")
-    console.log(err);
-});
+// connect.then(()=>{
+//     console.log("CONNECTED TO DATABASE...");
+// })
+// .catch((err)=>{
+//     console.log("FAILED TO CONNECT DATABASE...")
+//     console.log(err);
+// });
 
-module.exports = connect;
+// module.exports = connect;
